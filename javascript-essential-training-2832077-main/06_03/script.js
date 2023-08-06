8839c53d6bcc47e9b3d670511ae82d8ea4a17cf3
wendersonads <wendersonads5.5@gmail.com>
2023-08-06 15:49:45 -0300

    :ok_hand: alterações DOM

--- javascript-essential-training-2832077-main/06_03/script.js
+++ javascript-essential-training-2832077-main/06_03/script.js
@@ -7,13 +7,13 @@ var color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
-
+color = "yellow"
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
 
-// function headingColor() {
-//   color = "blue";
-//   document.querySelector(".title").style.color = color;
-// }
+function headingColor() {
+  color = "blue";
+  document.querySelector(".title").style.color = color;
+}
 
-// headingColor();
+headingColor();
